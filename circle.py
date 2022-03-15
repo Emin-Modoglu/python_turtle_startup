e3d61d740f9f4668ca1eb8790c9c41e1818482ec
Emin Modoğlu <96925303+Emin-Modoglu@users.noreply.github.com>
2022-03-15 20:47:16 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,4 @@
+import turtle     # pip install PythonTurtle kütüphanesi indiriyoruz ve import ile projeye dahil ediyoruz...
+ok = turtle.Turtle()
+
+ok.circle(100)  # ing = circle , tr = çember modülle birlikte gelen keywords'ler
